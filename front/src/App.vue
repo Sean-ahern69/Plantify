81b2f7adfe56d18ebed05944871bb04383fcc7f7
KJ Jenkins <99845714+KJinkies@users.noreply.github.com>
2022-04-04 17:42:28 +1200

    nav bar design

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
-
+import navbar from './components/NavBar.vue'
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
   
 <h1>Welcome</h1>
 
+<NavBar/>
+
 </template>
 
 <style>
+*{
+    margin:0;
+}
 
 </style>
--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,13 +1,67 @@
+
+
 <template>
-    <h1>nav bar</h1>
+
+    <div class="flex_container" >
+
+        <div class="flex_child"><img :src="homeicon" class="icon_class" ><span class="icon_label">Home</span></div>
+        <div class="flex_child"><img :src="listingicon" class="icon_class" > <span class="icon_label">Listings</span> </div>
+        <div class="flex_child"><img :src="watchlisticon" class="icon_class" > <span class="icon_label">Watchlist</span> </div>
+        <div class="flex_child"><img :src="accounticon" class="icon_class" ><span class="icon_label">Account</span></div>
+
+    </div>
+
 </template>
 
 <style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+
+.flex_container{
+    display:flex;
+    justify-content: space-around;
+    background-color: #73C088;
+    height: 90px;
+    width: 100%;
+    bottom: 0;
+    align-items: center;
+    position: fixed;         /* may need to come out once integrated with app*/
+}
+
+.flex_child{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.icon_class{
+  height:38px;
+}
+
+.icon_label{
+  font-family: 'Poppins', sans-serif;
+  font-size: 600;
+  color: white;
+  padding: 5px 0px 0px 0px;
+}
 
 </style>
 
 <script>
 
+/* sourcing the icon logos locally*/
+
+export default {
+  name: 'NavbarComponent',
+  data: function() {
+    return {
+      homeicon: './src/assets/home.png',
+      listingicon: './src/assets/listings.png',
+      watchlisticon: './src/assets/watchlist.png',
+      accounticon: './src/assets/account.png'
+    }
+  }
+}
 </script>
 
 <script setup>
