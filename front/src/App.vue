d71c3fa8ef4a2c388748d4ecf78ea36f4de10273
Mustafashaf2 <270068592@yoobeestudent.ac.nz>
2022-04-05 10:55:50 +1200

    Updated Tags

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
 
+import TagsVue from './components/home/Tags.vue';
+
 </script>
 
 <template>
   
-<h1>Welcome</h1>
+<tags-vue/>
 
 </template>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
 
 </style>
--- front/src/components/home/Tags.vue
+++ front/src/components/home/Tags.vue
@@ -1,8 +1,10 @@
 <template>
-    <h1>tags</h1>
+    <h1>Plantify</h1>
 </template>
     
 <style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
 
 </style>
 
