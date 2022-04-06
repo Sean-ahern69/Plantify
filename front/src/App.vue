fa88c9a4ac3126735395525303d47e267051528d
KJ Jenkins <99845714+KJinkies@users.noreply.github.com>
2022-04-06 15:30:09 +1200

    updated app.vue

---
+++ front/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+
+
+import NavBar from './components/NavBar.vue';
+</script>
+
+
+
+<template>
+
+
+
+
+<router-view></router-view>
+
+
+
+<NavBar/>
+
+
+
+</template>
+
+
+
+<style>
+*{
+margin: 0;
+}
+</style>
